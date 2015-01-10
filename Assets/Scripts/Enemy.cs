59ad909649943da5abf1377ac4c91a65df9dcd03
Mantas Palaima <palaima.mantas@gmail.com>
2015-01-10 02:26:07 +0200

    added enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float velocity = 1f;
+	Transform sightStart;
+	Transform sightEnd;
+	Transform weakness;
+	
+	bool colliding;
+	
+	Animator anim;
+	
+	public LayerMask detectWhat;
+
+
+	private bool facingRight = true;			// Nustatoma i kuria puse ziures
+	private Transform groundCheck = null;			// Pozicija nustatanti kur zaidejas liecia zeme
+	private float groundedRadius = .2f;			// Per koki atstuma nustato kur zeme
+	private Transform ceilingCheck = null;				// Pozicija nustatanti kur zaidejas liecia lubas
+	private float ceilingRadius = .1f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+
+
+	void Awake() {
+		// uzbindina
+		sightStart = transform.Find("sightStart");
+		sightEnd = transform.Find("sightEnd");
+		weakness = transform.Find("weakness");
+	}
+
+	void FixedUpdate() {
+		rigidbody2D.velocity = new Vector2 (velocity, rigidbody2D.velocity.y);
+		colliding = Physics2D.Linecast (sightStart.position, sightEnd.position, detectWhat);
+		
+		if (colliding) {
+			
+			transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
+			velocity*= -1;
+			
+		}
+		// Pass all parameters to the character control script.
+		//Move();
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Player") {
+			float height= col.contacts[0].point.y - weakness.position.y;
+			if(height>0) {
+				Dies();
+				col.rigidbody.AddForce(new Vector2(0,300));	
+			}	
+		}
+	}
+
+	void Dies() {
+		anim.SetBool ("stomped", true);
+		Destroy (this.gameObject, 0.5f);
+		gameObject.tag = "neutralized";	
+	}
+
+	void Move() {
+		/*if (facingRight) {
+			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+		} else {
+			rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);
+		}
+
+		/*if(move > 0 && !facingRight) {
+			Flip();
+		} else if(move < 0 && facingRight) {
+			Flip();
+		}*/	
+	}
+
+	/*
+	*	Metodas skirtas apsukti personaza
+	*/
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
