4d2660b6dfdf33deba956a439c2b3b5c3081f995
Mantas Palaima <palaima.mantas@gmail.com>
2015-01-11 02:13:46 +0200

    destroy coin box after collision

--- Assets/Scripts/shooter.cs
+++ Assets/Scripts/shooter.cs
@@ -7,20 +7,9 @@ public class shooter : MonoBehaviour {
 	public float speedFactor;
 	public float delay;
 	Transform weakness;
-
-
+	
 	void Awake() {
 		weakness = transform.Find("weakness");
-	}
-
-	// Use this for initialization
-	void Start () {
-		//StartCoroutine (Shoots ());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
@@ -28,8 +17,7 @@ public class shooter : MonoBehaviour {
 			float height= col.contacts[0].point.y - weakness.position.y;
 			if(height<0) {
 				GameObject clone = (GameObject)Instantiate(projectile, transform.position,Quaternion.identity);
-				//clone.rigidbody2D.velocity= -transform.right * speedFactor;
-				//clone.rigidbody2D.AddForce(new Vector2(20f,20f));
+				Destroy(this.gameObject);
 			}
 		}
 	}
